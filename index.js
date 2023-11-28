a5fdc9dff3b1e2f8836b2bf0f8ec80f7c050b877
Kate Mendeleeva <mendeleevakate2002@gmail.com>
2023-11-28 20:26:03 +0300

    made the logic of winning

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ let area = document.getElementById('area');
 let cell = document.getElementsByClassName('cell');
 let currentPlayer = document.getElementById('curPlayer')
 
+
 let player = 'player_x';
 
 
 let stat = {
-    'x': 0,
-    'o': 0,
-    'd': 0
+    'player_x': 0,
+    'player_o': 0,
+    'draw': 0
 };
 
 let winIndex = [
@@ -28,82 +29,94 @@ for (let i = 1; i <= 9; i++) {
 for (let i = 0; i < cell.length; i++) { 
     cell[i].addEventListener('click', cellClick, false)
 }
+
 function cellClick() {
     let data = [];
     
 
-    // Проверяем, есть ли у элемента класс "myClass"
+    
     if (!this.classList.contains(player)) {
         this.classList.add(player);
-        let img = document.createElement("img"); // Создаем новый элемент <img>
-        img.src = `img/${player}.svg`; // Устанавливаем путь к изображению
-        this.appendChild(img); // Добавляем изображение к текущему элементу
-        console.log("Элемент имеет класс 'myClass'");
+        let img = document.createElement("img"); 
+        img.src = `img/${player}.svg`; 
+        img.width = 150;
+        img.height = 150;
+        img.classList.add('img-player');
+        this.style.backgroundImage = 'none'
+        this.appendChild(img); 
+        
     } else {
         alert('ячейка занята')
         return;
     }
 
-    // if (!this.innerHTML) {
-    //     this.innerHTML = player;
-    // } else { 
-    //     alert('ячейка занята')
-    //     return;
-    // }
-
-    // for (let i in cell) { 
-    //     if (cell[i].innerHTML == player) { 
-    //         data.push(parseInt(cell[i].getAttribute('pos')));
-    //     }
-    // }
+    for (let cellElement of cell) {
+        if (cellElement.classList.contains(player)) {
+            data.push(parseInt(cellElement.getAttribute('pos')));
+        }
+    }
+    console.log(data);
     
-    // if (CheckWin(data)) {
-    //     stat[player] += 1;
-    //     restart('Выиграл:' + player)
-    // } else { 
-    //     let draw = true;
-    //     for (let i in cell) { 
-    //         if (cell[i].innerHTML == '') { 
-    //             draw = false;
-    //         }
-    //     }
-    //     if (draw) { 
-    //         stat.d += 1;
-    //         restart('ничья')
-    //     }
-    // }
+    
+    if (CheckWin(data)) {
+        stat[player] += 1;
+        restart('Выиграл:' + player)
+    }
+    //else { 
+        //      let draw = true;
+        //      for (let i in cell) {
+                
+        //      }
+        
+        //     if (draw) {
+        //         stat.draw += 1;
+        //        restart('ничья')
+        //   }
+        // }
     player = player == 'player_x' ? 'player_o' : 'player_x';
     currentPlayer.innerHTML = player.toUpperCase();
+ 
+   
 }
 
-    // function CheckWin(data) {
-    //     for (let i in winIndex) { 
-    //         let win = true;
-    //         for (let j in winIndex[i]) {
-    //             let id = winIndex[i][j];
-    //             let ind = data.indexOf(id);
+function CheckWin(data) {
+    for (let i in winIndex) { 
+        let win = true;
+        for (let j in winIndex[i]) {
+            let id = winIndex[i][j];
+            let ind = data.indexOf(id);
 
-    //             if (ind == -1) {
-    //                 win = false
-    //             }
-    //         }
+            if (ind == -1) {
+                win = false
+            }
+        }
 
-    //         if (win) return true
-    //     }
-    //     return false
-    // }
+        if (win) return true
+    }
+    return false
+}
 
-    // function restart(text) {
-    //     alert(text);
-    //     updateStat();
-    //     for (let i =0 ; i <= cell.length; i++) {
-    //         cell[i].innerHTML = '';
-    //     }
+    function restart(text) {
+        alert(text);
+        updateStat()
+        let images = document.getElementsByClassName('img-player');
         
-    // }
-    // function updateStat() {
-    //     document.getElementById('sX').innerHTML = stat.x;
-    //     document.getElementById('sO').innerHTML = stat.o;
-    //     document.getElementById('sD').innerHTML = stat.d;
-    // }
+        while (images.length > 0) {
+
+            let parentCell = images[0].parentNode;
+            parentCell.removeChild(images[0]);
+            parentCell.classList.remove('player_x');
+            parentCell.classList.remove('player_o');
+            parentCell.style.backgroundImage = 'url(../img/Neon-border.svg';
+        }
+
+    }
+    function updateStat() {
+        document.getElementById('sX').innerHTML = stat.player_x;
+        document.getElementById('sO').innerHTML = stat.player_o;
+        document.getElementById('sD').innerHTML = stat.draw;
+    }
+
+
+
 
